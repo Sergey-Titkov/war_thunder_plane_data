6fdf026dd1c85b39710be13da6cd352fdcfe4a16
Sergey Titkov <Srg.titkov@gmail.com>
2025-04-29 13:46:38 +0300

    Теперь стало удобно обновлять

--- make_file_for_wtmfd.py
+++ make_file_for_wtmfd.py
@@ -2,24 +2,40 @@ import glob
 import os
 import war_thunder_plane_info.wt as wt
 import json
-
+from packaging import version
+import shutil
 # Список самолетов который нам интересен, если пусто, то обработаем все
 list_plane = [
     'er-2_m105_tat'
     ]
 
 if __name__ == "__main__":
-    # Каталог откуда считываем флат модели
-    res = glob.glob(fr'./War-Thunder-Datamine-master/aces.vromfs.bin_u/gamedata/flightmodels/*.blkx')
-    # Нет, нет и нет
-    exclude = ['h_81a_2']
-    result_json = {}
-    # Список файлов есть, пошли по нему
-    for file in res:
-        plane_id = os.path.basename(file).replace('.blkx', '')
-        if plane_id not in exclude:
-            plane_datamine = wt.WTPlaneFullInfo(plane_id)
-            result_json[plane_id]=plane_datamine.get_all()
+    with open(fr'./War-Thunder-Datamine-master/version', 'r', encoding="utf-8") as file:
+        datamine_version = version.parse(file.read())
 
-    with open('wtmfd_data.json', 'w', encoding="utf-8") as file:
-        json.dump(result_json, file, ensure_ascii=False)
+    with open('wtmfd_data_version.json', 'r', encoding="utf-8") as file:
+        json_version = version.parse(json.load(file)["Version"])
+
+    # У нас новые данные по флайт модели, обновляемся
+    if datamine_version > json_version:
+        shutil.copy2('wtmfd_data.json', f'wtmfd_data {json_version}.json')
+        shutil.copy2('wtmfd_data_version.json', f'wtmfd_data_verion {json_version}.json')
+
+        # Каталог откуда считываем флат модели
+        res = glob.glob(fr'./War-Thunder-Datamine-master/aces.vromfs.bin_u/gamedata/flightmodels/*.blkx')
+        # Нет, нет и нет
+        exclude = ['h_81a_2']
+        result_json = {}
+        # Список файлов есть, пошли по нему
+        for file in res:
+            plane_id = os.path.basename(file).replace('.blkx', '')
+            if plane_id not in exclude:
+                plane_datamine = wt.WTPlaneFullInfo(plane_id)
+                result_json[plane_id]=plane_datamine.get_all()
+
+        with open('wtmfd_data.json', 'w', encoding="utf-8") as file:
+            json.dump(result_json, file, ensure_ascii=False, indent=4)
+
+        result_json = {"Version":f"{datamine_version}"}
+        with open('wtmfd_data_version.json', 'w', encoding="utf-8") as file:
+            json.dump(result_json, file, ensure_ascii=False, indent=4)
